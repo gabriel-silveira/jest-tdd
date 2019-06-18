e2e0d3fc7489db7dff938dfa42f9de29e78ea61e
Gabriel Silveira <gabriel@gabrielsilveira.com.br>
2019-06-18 19:21:02 -0300

    Jest config

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,3 @@
 const routes = require('express').Router
-const { User } = require('./app/models')
-
-User.create({
-  name: 'Gabriel Siveira',
-  email: 'gabriel@gabrielsilveira.com.br',
-  password_hash: '123412451543'
-})
 
 module.exports = routes
